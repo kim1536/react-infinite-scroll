e2c5db06cc5bc574339d99ff70b2808dd7a63c13
kimkwonil <rnjsdlf0322@gmail.com>
2023-10-05 12:18:00 +0900

    convertTimeFormat 修正

--- src/utils/ConvertTimeFormat.js
+++ src/utils/ConvertTimeFormat.js
@@ -5,17 +5,22 @@ export const convertTimeFormat = (items) => {
     const timeDifference = now - targetDate;
     const minutesDifference = Math.floor(timeDifference / (1000 * 60));
     const hoursDifference = Math.floor(minutesDifference / 60);
+    const daysDifference = Math.floor(hoursDifference / 24);
 
     let displayText = '';
 
-    if (minutesDifference < 60) {
-      displayText = `${minutesDifference}m ago`;
-    } else {
+    if (daysDifference === 1) {
+      displayText = 'yesterday';
+    } else if (daysDifference > 1) {
+      displayText = `${daysDifference} days ago`;
+    } else if (hoursDifference >= 1) {
       displayText = `${hoursDifference}h ago`;
+    } else {
+      displayText = `${minutesDifference}m ago`;
     }
 
     return { ...post, created_at: displayText };
   });
 
-  return newItem
+  return newItem;
 }
